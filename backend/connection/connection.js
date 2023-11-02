fbb3dba3e8fcb87ae6c29d02187307f4f5611142
Camila Jaurena <jaurenacamila@gmail.com>
2023-11-02 15:43:47 -0300

    config

--- backend/connection/connection.js
+++ backend/connection/connection.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose';
+import { dbPort, dbName } from '../config/config.js'
 
 async function connectToDatabase() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test');
-    // Use this line if your database has authentication enabled
-    // await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');
-    console.log('Connected to MongoDB');
+    await mongoose.connect(`mongodb://127.0.0.1:${dbPort}/${dbName}`);
+    //mongodb://<hostname>:<port>/<database> -> 'mongodb://127.0.0.1:27017/test'
+    //Use this line if your database has authentication enabled: await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');
+    console.log('Db port:', dbPort);
+    console.log('Db name:', dbName)
   } catch (err) {
     console.error('MongoDB connection error:', err);
   }
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,22 @@
 import express from 'express'
 import { connectToDatabase } from './connection/connection.js'
+import { serverPort } from './config/config.js'
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, Express with MongoDB!');
+    res.send('you are on /');
 });
 
 async function startServer() {
     try {
       await connectToDatabase();
-      console.log('Connected to the database');
-      app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
+      app.listen(serverPort, () => {
+        console.log('Server port:', serverPort);
       });
     } catch (err) {
       console.error('Error starting the server:', err);
     }
   }
   
-  startServer();
+startServer();
